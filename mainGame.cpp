abe0e1e884cf8ff7a66a0b26e78d80fcc41ee0c4
HammadAli132 <alihammad132@outlook.com>
2024-05-01 23:19:19 +0500

    Made the maze using a single maze png

--- mainGame.cpp
+++ mainGame.cpp
@@ -4,19 +4,28 @@
 using namespace std;
 using namespace sf;
 
-void *GAMEINIT(void *arg) {
-    RenderWindow gameWindow(VideoMode(500.0f, 500.0f), "PACMAN Game", Style::Default);
-    RectangleShape rectangle(Vector2f(50.0f, 100.0f));
-    rectangle.setFillColor(Color::Blue);
+#define GRIDHEIGHT 800
+#define GRIDWIDTH 1000
+#define CELLSize 25
+const int gridRows = GRIDHEIGHT / CELLSize;
+const int gridCols = GRIDWIDTH / CELLSize;
+
+void *GAMEINIT(void *arg) { // main game thread
+    RenderWindow gameWindow(VideoMode(GRIDWIDTH, GRIDHEIGHT), "PACMAN Game", Style::Default);
+    Texture maze; // creating a texture for maze.png
+    maze.loadFromFile("sprites/grid.png"); // loading the texture with maze.png
+    Sprite gameGrid; // creating a sprite for Game Grid
+    gameGrid.setTexture(maze); // setting the Game Grid Sprite to maze texture
+    gameGrid.scale(CELLSize, CELLSize); // scaling the sprite accoring to the cell size to fit in the screen
     while (gameWindow.isOpen()) {
         Event event;
-        while (gameWindow.pollEvent(event)) {
+        while (gameWindow.pollEvent(event)) { // checking for window close command
             if (event.type == Event::Closed)
                 gameWindow.close();
         }
-        gameWindow.clear();
-        gameWindow.draw(rectangle);
-        gameWindow.display();
+        gameWindow.clear(); // clearing the buffer window
+        gameWindow.draw(gameGrid); // drawing the sprite to buffer window
+        gameWindow.display(); // swapping the buffer window with main window
     }
     pthread_exit(NULL);
 }
@@ -24,7 +33,7 @@ void *GAMEINIT(void *arg) {
 int main()
 {
     pthread_t startGame;
-    pthread_create(&startGame, NULL, GAMEINIT, NULL);
-    pthread_join(startGame, NULL);
+    pthread_create(&startGame, NULL, GAMEINIT, NULL); // initiating main game thread
+    pthread_join(startGame, NULL); // waiting for game thread to exit
     return 0;
 }
