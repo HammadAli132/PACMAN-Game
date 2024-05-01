079bfd4d31e5fb113239ce22a80935a359214387
HammadAli132 <alihammad132@outlook.com>
2024-05-02 02:46:20 +0500

    Implemented proper 2D game grid with maze and food displayed in it

--- mainGame.cpp
+++ mainGame.cpp
@@ -4,19 +4,63 @@
 using namespace std;
 using namespace sf;
 
-#define GRIDHEIGHT 800
-#define GRIDWIDTH 1000
-#define CELLSize 25
+#define GRIDHEIGHT 850
+#define GRIDWIDTH 1050
+#define CELLSize 50
 const int gridRows = GRIDHEIGHT / CELLSize;
 const int gridCols = GRIDWIDTH / CELLSize;
 
+vector<vector<int>> maze1 = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1},
+    {1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1},
+    {1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1},
+    {1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1},
+    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1},
+    {1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1},
+    {1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1},
+    {1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+};
+
+void DRAWMAZE(RenderWindow &window, Sprite food, Sprite mazeBox) { // this function draws the maze
+    for (int i = 0; i < gridRows; i++) {
+        for (int j = 0; j < gridCols; j++) {
+            if (maze1[i][j] == 1) {
+                mazeBox.setPosition(j * CELLSize, i * CELLSize); // setting position of mazeBox
+                window.draw(mazeBox); // drawing the mazeBox on window so that it can be rendered
+            }
+            if (maze1[i][j] == 0) {
+                food.setPosition(j * CELLSize + (CELLSize / 2), i * CELLSize + (CELLSize / 2)); // setting position of food such that it appears in the center of its block
+                window.draw(food); // drawing the food on window so that it can be rendered
+            }
+        }
+    }
+    return;
+}
+
 void *GAMEINIT(void *arg) { // main game thread
     RenderWindow gameWindow(VideoMode(GRIDWIDTH, GRIDHEIGHT), "PACMAN Game", Style::Default);
-    Texture maze; // creating a texture for maze.png
-    maze.loadFromFile("sprites/grid.png"); // loading the texture with maze.png
-    Sprite gameGrid; // creating a sprite for Game Grid
-    gameGrid.setTexture(maze); // setting the Game Grid Sprite to maze texture
-    gameGrid.scale(CELLSize, CELLSize); // scaling the sprite accoring to the cell size to fit in the screen
+
+    Texture box; // creating a texture for maze.png
+    box.loadFromFile("sprites/box.png"); // loading the texture with maze.png
+    Sprite mazeBox; // creating a sprite for Game Grid
+    mazeBox.setTexture(box); // setting the Game Grid Sprite to maze texture
+    mazeBox.scale(CELLSize, CELLSize); // scaling the sprite accoring to the cell size to fit in the screen
+
+    Texture food; // creating a texture for food.png
+    food.loadFromFile("sprites/food.png"); // loading the texture with food.png
+    Sprite foodBox; // creating a sprite for food
+    foodBox.setTexture(food); // setting the foodBox Sprite to food texture
+    foodBox.scale(CELLSize / 4, CELLSize / 4);
+
     while (gameWindow.isOpen()) {
         Event event;
         while (gameWindow.pollEvent(event)) { // checking for window close command
@@ -24,9 +68,11 @@ void *GAMEINIT(void *arg) { // main game thread
                 gameWindow.close();
         }
         gameWindow.clear(); // clearing the buffer window
-        gameWindow.draw(gameGrid); // drawing the sprite to buffer window
+        // gameWindow.draw(mazeBox); // drawing the sprite to buffer window
+        DRAWMAZE(gameWindow, foodBox, mazeBox);
         gameWindow.display(); // swapping the buffer window with main window
     }
+
     pthread_exit(NULL);
 }
 
